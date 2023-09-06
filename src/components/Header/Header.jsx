f268bd9e4bb7e42b63dd480eddd817026d4bfb0a
tryit-100 <safwan.std21@gmail.com>
2023-09-06 16:11:50 +0600

    header component ui is done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    
+    return (
+        <nav className="header">
+            <img src={logo} />
+            <div className="nav-items">
+                <a href="/order">Order</a>
+                <a href="/orderReview">Order Review</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
