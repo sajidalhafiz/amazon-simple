5648ef13da6c75e93f74a093991bdd3ba2c88959
tryit-100 <safwan.std21@gmail.com>
2023-10-12 15:49:49 +0600

    react-router is initiated and all is working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css';
 import logo from '../../images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     
@@ -7,10 +8,10 @@ const Header = () => {
         <nav className="header">
             <img src={logo} />
             <div className="nav-items">
-                <a href="/shop">Shop</a>
-                <a href="/orderReview">Order Review</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/login">Login</a>
+                <Link to="/">Shop</Link>
+                <Link to="/orders">Order Review</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
         </nav>
     );
